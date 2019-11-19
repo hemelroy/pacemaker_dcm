bb84856c38389e94f4d5d4c6f2885040db304d62
Ayman Shaikh <aymanshaikh@gmail.com>
2019-11-19 17:59:44 -0500

    serial transmit error fixed

--- serialComm.py
+++ serialComm.py
@@ -1,13 +1,16 @@
-import serial 
+from serial import Serial
 import time
 
-def serialTransmit(paramaters):
+ser = Serial('COM7', baudrate=115200, timeout=1)
+
+
+def serialTransmit(parameters):
 	list_serialOut = [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 22]
-	for i in range (0: len(parameters)):
+	for i in range(0, len(parameters)):
 		if i == 0 or i ==1:
-			parameters [i] = round(1/(paramaters [i]) * 60 *1000)
-		list_serialOut[2+i] = paramaters [i]
-		
+			parameters [i] = round(1/(int(parameters [i])) * 60 *1000)
+		list_serialOut[2+i] = parameters [i]
+	print (list_serialOut)
 	#index 0: always 1
 	#index 1: 1 for transmit, 0 for receive
 	#index 2 through 20: programmable parameters
@@ -16,4 +19,3 @@ def serialTransmit(paramaters):
 		ser.write(bytes([i]))
 	time.sleep(1)
 
-ser = serial.Serial('COM5', baudrate=115200, timeout=1)
